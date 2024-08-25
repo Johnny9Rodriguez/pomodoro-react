0501925c249ad9fc8675c6659594e002f3b1a3fe
Johnny9Rodriguez <mail@joepytlik.de>
2024-08-25 06:04:48 +0200

    add circle progression bar

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { FiCrosshair, FiCoffee } from 'react-icons/fi';
 import Options from './Options';
 
@@ -6,15 +6,24 @@ function Clock() {
     const [time, setTime] = useState(null);
     const [displayTime, setDisplayTime] = useState(null);
     const [isWork, setIsWork] = useState(true);
+    const [deg, setDeg] = useState(0);
+    const timeRef = useRef(null);
 
     useEffect(() => {
         const getInitTime = async () => {
             const initTime = await window.pomodoro.getTime();
             setTime(initTime);
+            timeRef.current = initTime;
         };
         getInitTime();
-        window.pomodoro.onUpdateTime((time) => setTime(time));
-        window.pomodoro.onUpdateTimer((count) => setIsWork(count % 2 === 0));
+        window.pomodoro.onUpdateTime((newTime) => {
+            setTime(newTime);
+        });
+        window.pomodoro.onUpdateTimer((count, newTime) => {
+            setIsWork(count % 2 === 0);
+            setTime(newTime);
+            timeRef.current = newTime;
+        });
     }, []);
 
     useEffect(() => {
@@ -27,8 +36,12 @@ function Clock() {
 
             setDisplayTime(`${dMin}:${dSec}`);
         };
-
         renderDisplayTime();
+
+        if (timeRef.current) {
+            const deg = (360 / timeRef.current) * (timeRef.current - time);
+            setDeg(deg);
+        }
     }, [time]);
 
     return (
@@ -55,7 +68,12 @@ function Clock() {
             <div className='absolute z-40 p-1.5 w-full h-full'>
                 <div className='w-full h-full rounded-full bg-purple-800' />
             </div>
-            <div className='absolute z-30 w-full h-full rounded-full' />
+            <div
+                className='absolute z-30 w-full h-full rounded-full'
+                style={{
+                    background: `conic-gradient(transparent, #9333ea ${deg}deg, transparent ${deg}deg)`,
+                }}
+            />
             <div className='absolute z-20 w-full h-full rounded-full bg-purple-200' />
             <Options />
         </div>
